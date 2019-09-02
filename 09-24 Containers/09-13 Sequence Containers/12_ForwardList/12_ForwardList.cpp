8e55b898a1b6bb797fd1522e41dcc47148ea42bb
Orest Brykowytch <22brykowytch_o@aswarsaw.org>
2019-09-02 10:49:04 +0200

    Constructor/2 Member Functions
    constructor, assign(), before_begin()

---
+++ 09-24 Containers/09-13 Sequence Containers/12_ForwardList/12_ForwardList.cpp
@@ -0,0 +1,10 @@
+#include "12_ForwardList.h"
+
+int main()
+{
+    creatingForward_lists();
+    flAssign();
+    flBefore_Begin();
+
+    return 0;
+}
---
+++ 09-24 Containers/09-13 Sequence Containers/12_ForwardList/12_ForwardList.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <forward_list>
+#include <utility>
+
+void creatingForward_lists()
+{
+    std::cout << "Basic forward_list setup: " << std::endl;
+
+    std::forward_list<int> flOne;
+    std::forward_list<int> flTwo (5, 5);
+    std::forward_list<int> flThree (flTwo.begin(), flTwo.end());
+    std::forward_list<int> flFour (flThree);
+    std::forward_list<int> flFive (std::move(flFour));
+
+    std::cout << "flOne contains:";
+    for (std::forward_list<int>::iterator x = flOne.begin(); x != flOne.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flTwo contains:";
+    for (std::forward_list<int>::iterator x = flTwo.begin(); x != flTwo.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flThree contains:";
+    for (std::forward_list<int>::iterator x = flThree.begin(); x != flThree.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flFour contains:";
+    for (std::forward_list<int>::iterator x = flFour.begin(); x != flFour.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flFive contains:";
+    for (std::forward_list<int>::iterator x = flFive.begin(); x != flFive.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << '\n' << std::endl;
+}
+
+void flAssign()
+{
+    std::cout << "std::deque::assign()" << std::endl;
+
+    std::forward_list<int> flOne;
+    std::forward_list<int> flTwo;
+    std::forward_list<int> flThree;
+
+    flOne.assign(5, 10);
+
+    flTwo.assign(flOne.begin(), flOne.end());
+
+    flThree.assign({1, 2, 3, 4, 5 });
+
+    std::cout << "flOne contains:";
+    for (std::forward_list<int>::iterator x = flOne.begin(); x != flOne.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flTwo contains:";
+    for (std::forward_list<int>::iterator x = flTwo.begin(); x != flTwo.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    std::cout << "flThree contains:";
+    for (std::forward_list<int>::iterator x = flThree.begin(); x != flThree.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << '\n' << std::endl;
+}
+
+void flBefore_Begin()
+{
+    std::forward_list<int> flOne = { 1, 2, 3, 4, 5 };
+
+    std::cout << "flOne contains:";
+    for (std::forward_list<int>::iterator x = flOne.begin(); x != flOne.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << std::endl;
+
+    flOne.insert_after(flOne.before_begin(), 10);
+
+    std::cout << "flOne now contains:";
+    for (std::forward_list<int>::iterator x = flOne.begin(); x != flOne.end(); ++x)
+    {
+        std::cout << " " << *x;
+    }
+    std::cout << '\n' << std::endl;
+}
